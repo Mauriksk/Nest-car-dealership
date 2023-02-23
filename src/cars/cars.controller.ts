b8f6f709fab4d834dd69b98338233c530204c5dd
Mauricio Torres <otarksk@gmail.com>
2023-02-23 15:56:20 -0300

    Use of Get decorator

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,20 +1,22 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common/pipes';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
     
+    constructor(
+        private readonly carsService: CarsService
+    ){}
 
     @Get()
     get(){
-        return this.cars
+        return this.carsService.findAll()
     }
 
     @Get(':id')
-    getCarById( @Param('id') id: string) {
-        return {
-            id,
-            car: this.cars[+id]
-        }
+    getCarById( @Param('id', ParseIntPipe) id: number) {
+        return this.carsService.findOneById(id)
     }
 }
 
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class CarsService {
@@ -7,4 +8,14 @@ export class CarsService {
     { id: 2, brand: 'Honda' },
     { id: 3, brand: 'Jeep' },
   ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    if(!car) throw new NotFoundException(`Car with id '${id}' not found`);
+    return car;
+  }
 }
