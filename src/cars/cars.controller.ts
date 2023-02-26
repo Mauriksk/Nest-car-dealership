eda588c96a58766a85d98e54b3025e0bb2afd765
Mauricio Torres <otarksk@gmail.com>
2023-02-26 20:13:39 -0300

    Creating DTO

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, ParseUUIDPipe, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -14,14 +15,14 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById( @Param('id' ) id: string ) {
+  getCarById( @Param('id', new ParseUUIDPipe({version: '4'}) ) id: string ) {
 
     return this.carsService.findOneById( id );
   }
 
   @Post()
-  createCar( @Body() body: any ) {
-    return body;
+  createCar( @Body() createCarDto: CreateCarDto ) {
+    return createCarDto;
   }
 
   @Patch(':id')
