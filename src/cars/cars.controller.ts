99de06c4f089cdbae0b7d50feb6b84896bd4da3d
Mauricio Torres <otarksk@gmail.com>
2023-02-28 10:18:12 -0300

    Add Post and save in array

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,45 +1,47 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, ParseUUIDPipe, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  ParseUUIDPipe,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
-  
-  constructor(
-    private readonly carsService: CarsService
-  ) {}
+  constructor(private readonly carsService: CarsService) {}
 
   @Get()
   getAllCars() {
-    return this.carsService.findAll()
+    return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById( @Param('id', new ParseUUIDPipe({version: '4'}) ) id: string ) {
-
-    return this.carsService.findOneById( id );
+  getCarById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.carsService.findOneById(id);
   }
 
   @Post()
-  createCar( @Body() createCarDto: CreateCarDto ) {
-    return createCarDto;
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return this.carsService.create(createCarDto);
   }
 
   @Patch(':id')
-  updateCar( 
-    @Param('id') id: string, 
-    @Body() body: any ) 
-  {
+  updateCar(@Param('id') id: string, @Body() body: any) {
     return body;
   }
 
   @Delete(':id')
-  deleteCar( @Param('id' ) id: string ) {
+  deleteCar(@Param('id') id: string) {
     return {
       method: 'delete',
-      id
+      id,
     };
   }
-
-
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,40 +1,47 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from 'uuid';
 import { Car } from './interface/car.interface';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Injectable()
 export class CarsService {
+  private cars: Car[] = [
+    {
+      id: uuid(),
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: uuid(),
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: uuid(),
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
 
-    private cars: Car[] = [
-        {
-            id: uuid(),
-            brand: 'Toyota',
-            model: 'Corolla' 
-        },
-        {
-            id: uuid(),
-            brand: 'Honda',
-            model: 'Civic' 
-        },
-        {
-            id: uuid(),
-            brand: 'Jeep',
-            model: 'Cherokee' 
-        },
-    ];
+  findAll() {
+    return this.cars;
+  }
 
+  findOneById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+    if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
 
-    findAll() {
-        return this.cars;
-    }
+    return car;
+  }
 
-    findOneById( id: string ) {
-        
-        const car = this.cars.find( car => car.id === id );
-        if ( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
-        
-        return car;
-    }
+  create(createCarDto: CreateCarDto) {
+    const car = {
+      id: uuid(),
+      ...createCarDto,
+    };
 
+    this.cars.push(car);
+    return car;
+  }
 }
